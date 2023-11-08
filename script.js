5fcb419c9b10cec959f4c9c6d9bb5fa9d319d2bf
ketley <105372359+ketleycelest2510@users.noreply.github.com>
2023-11-08 08:06:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,71 +14,71 @@ function botao_resposta() {
     if(respostausuario1 && respostausuario2 && respostausuario3 && respostausuario4 && respostausuario5 && respostausuario6 && respostausuario7 && respostausuario8 && respostausuario9 && respostausuario10){
         
         if(respostausuario1.value === "correta"){
-            document.getElementById("resultado_1").innerHTML = "acertou";
+            document.getElementById("resultado_1").innerHTML = "acertous!!! Parabens";
         }
         else{
-            document.getElementById("errada1").innerHTML = "errada";
+            document.getElementById("errada1").innerHTML = "errada!!! A resposta certa é a C. Um político que exerceu a presidência do Brasil em duas ocasiões.";
         }
         if(respostausuario2.value === "correta"){
-            document.getElementById("resultado_2").innerHTML = "acertou";
+            document.getElementById("resultado_2").innerHTML = "acertou!!! Parabens";
         }
         else{
-            document.getElementById("errada2").innerHTML = "errada";
+            document.getElementById("errada2").innerHTML = "errada!!! A resposta certa é a B.A Revolução de 1930.";
         }
         if(respostausuario3.value === "correta"){
-            document.getElementById("resultado_3").innerHTML = "acertou";
+            document.getElementById("resultado_3").innerHTML = "acertou!!! Parabens";
         }
         else{
-            document.getElementById("errada3").innerHTML = "errada";
+            document.getElementById("errada3").innerHTML = "errada!!! A resposta certa é a  B. Um regime autoritário com censura à imprensa e restrições às liberdades civis.";
         }
         if(respostausuario4.value === "correta"){
-            document.getElementById("resultado_4").innerHTML = "acertou";
+            document.getElementById("resultado_4").innerHTML = "acertou!!! Parabens";
         }
         else{
-            document.getElementById("errada4").innerHTML = "errada";
+            document.getElementById("errada4").innerHTML = "errada!!! A resposta certa é a B. Foi criada a CLT (Consolidação das Leis do Trabalho) que estabeleceu direitos trabalhistas.";
         }
         if(respostausuario5.value === "correta"){
-            document.getElementById("resultado_5").innerHTML = "acertou";
+            document.getElementById("resultado_5").innerHTML = "acertou!!! Parabens";
         }
         else{
-            document.getElementById("errada5").innerHTML = "errada";
+            document.getElementById("errada5").innerHTML = "errada!!! A resposta certa é a C.Pressões políticas e militares que o forçaram a renunciar.";
         }
         if(respostausuario6.value === "correta"){
-            document.getElementById("resultado_6").innerHTML = "acertou";
+            document.getElementById("resultado_6").innerHTML = "acertou!!! Parabens";
         }
         else{
-            document.getElementById("errada6").innerHTML = "errada";
+            document.getElementById("errada6").innerHTML = "errada!!! A resposta certa é a  A.Os Aliados.";
         }
         if(respostausuario7.value === "correta"){
-            document.getElementById("resultado_7").innerHTML = "acertou";
+            document.getElementById("resultado_7").innerHTML = "acertou!!! Parabens";
         }
         else{
-            document.getElementById("errada7").innerHTML = "errada";
+            document.getElementById("errada7").innerHTML = "errada!!! A resposta certa é a B. Indústria e urbanização.";
         }
         if(respostausuario8.value === "correta"){
-            document.getElementById("resultado_8").innerHTML = "acertou";
+            document.getElementById("resultado_8").innerHTML = "acertou!!! Parabens";
         }
         else{
-            document.getElementById("errada8").innerHTML = "errada";
+            document.getElementById("errada8").innerHTML = "errada!!! A resposta certa é a A.Foi promovida a industrialização, com o Estado desempenhando um papel ativo na economia.";
         }
         if(respostausuario9.value === "correta"){
-            document.getElementById("resultado_9").innerHTML = "acertou";
+            document.getElementById("resultado_9").innerHTML = "acertou!!! Parabens";
         }
         else{
-            document.getElementById("errada9").innerHTML = "errada";
+            document.getElementById("errada9").innerHTML = "errada!!! A resposta certa é a C.A Petrobras (Petróleo Brasileiro S.A.).";
         }
         if(respostausuario10.value === "correta"){
-            document.getElementById("resultado_10").innerHTML = "acertou";
+            document.getElementById("resultado_10").innerHTML = "acertou!!! Parabens";
         }
         else{
-            document.getElementById("errada10").innerHTML = "errada";
+            document.getElementById("errada10").innerHTML = "errada!!! A resposta certa é a D. Cometeu suicídio devido a pressões políticas e protestos.";
         }
     }
     else{
-        alert("Não deixar questões em aberto!");
+        alert("Não deixar questões em aberto!");/*alertar que deixou questoes em branco*/
     }
 }
-function ola_usuario(){
+function ola_usuario(){/*para colocar o nome do usuario*/
     var nome_usu = prompt ("Escreva seu nome:");
     if(nome_usu){
         document.getElementById("nome_usuario").innerHTML = nome_usu;
